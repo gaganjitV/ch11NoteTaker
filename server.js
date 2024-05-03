a92a3bc413863c2906d3e1e10f7738fe621c3a76
Gaganjit Virdi <gaganjitvirdi@MacBook-Pro-2.local>
2024-05-02 20:48:10 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")  // Import Express.js
+const path = require('path'); // Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const fs = require('fs');
+
+
+const PORT = 3001; // Set the port for the server to listen on
+const app = express() // Create an instance of the Express.js server
+
+
+app.use(express.static('public'));
+app.use(express.json());
+
+// GET route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
+// GET route for notes entry page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
